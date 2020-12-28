13ac11f0d8da0bcf09a5a9ca197bdd82c7b6ddc7
whatwewant <tobewhatwewant@gmail.com>
2020-12-28 10:48:29 +0800

    fix(core): fix maxImage calculate when min.X > viewport.X, should rethink it

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,45 @@
 import { setStyle } from '@zcorky/dom';
 
+export function getViewport() {
+  return {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+} 
+
 export function maxImage($img) {
+  const viewport = getViewport();
   const width = $img.width;
   const height = $img.height;
 
+  // 图片本身宽度 vs 高度
+  //  设置长边为最大
   if (width >= height) {
+    // 图片短边 vs 视口
+    // 
+    //  如果短边比视口还高（宽），
+    //    那么应该设置短边为最大宽度，
+    //    以保证长边不会突出
+    //
+    if (height >= viewport.height) {
+      return {
+        auto: 'width',
+        max: 'height',
+      };
+    }
+
     return {
       auto: 'height',
       max: 'width',
     };
   } else {
+    if (width >= viewport.height) {
+      return {
+        auto: 'height',
+        max: 'width',
+      };
+    }
+
     return {
       auto: 'width',
       max: 'height',
